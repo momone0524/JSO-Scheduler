6b9dbd65aa28ae9a59c3fc2402c1e5fa6c091669
Momone Takahashi <momone.takahash@smail.astate.edu>
2026-03-20 14:59:01 -0500

    Update Event from Poll Model

--- src/models/PollModel.ts
+++ src/models/PollModel.ts
@@ -5,6 +5,7 @@ import { User } from '../entities/User.js';
 import { CreatePollInput } from '../validators/PollValidator.js';
 
 const PollRepository = AppDataSource.getRepository(Poll);
+const EventRepository = AppDataSource.getRepository(Event);
 
 async function getAllPolls(): Promise<Poll[]> {
   return PollRepository.find({
@@ -81,4 +82,50 @@ async function addPoll(data: CreatePollInput, user: User, event: Event): Promise
   return PollRepository.save(newPoll);
 }
 
-export { addPoll, getAllPolls, getPollById, getPollInEvent };
+async function updateEventFromPoll(pollId: string): Promise<Event | null> {
+  const poll = await PollRepository.findOne({
+    where: { pollId },
+    relations: ['pollOptions', 'event'],
+    select: {
+      pollId: true,
+      title: true,
+      description: true,
+      closeAt: true,
+      isClosed: true,
+      pollType: true,
+      pollOptions: {
+        optionId: true,
+        joboption: true,
+        scheduleoption: true,
+        isWinner: true,
+      },
+      event: {
+        eventId: true,
+        eventName: true,
+        place: true,
+        date: true,
+        startTime: true,
+        endTime: true,
+      },
+    },
+  });
+
+  if (!poll) {
+    return null;
+  }
+
+  if (poll.pollType === 'job') {
+    return null;
+  }
+
+  const winner = poll.pollOptions.find((win) => win.isWinner);
+
+  if (!winner) {
+    return null;
+  }
+
+  poll.event.date = winner.scheduleoption;
+  return EventRepository.save(poll.event);
+}
+
+export { addPoll, getAllPolls, getPollById, getPollInEvent, updateEventFromPoll };
