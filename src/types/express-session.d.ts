09ce3555d36c98be0a037f03c314bb1137901952
Momone Takahashi <momone.takahash@smail.astate.edu>
2026-04-16 14:10:17 -0500

    templete changed

--- src/types/express-session.d.ts
+++ src/types/express-session.d.ts
@@ -5,12 +5,12 @@ declare module 'express-session' {
     clearSession(): Promise<void>; // DO NOT MODIFY THIS!
 
     // NOTES: Add your app's custom session properties here:
-    authenticatedUser: {
+    authenticatedUser?: {
       userId: string;
       email: string;
-      role: string;
+      displayName: string;
     };
-    isLoggedIn: boolean;
-    logInAttempts: number;
+    isLoggedIn?: boolean;
+    logInAttempts?: number;
   }
 }
